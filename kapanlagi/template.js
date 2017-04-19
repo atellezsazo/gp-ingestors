e8318acc38ed9213711d5bfc409161b29650e482
Carlos Caal <cammend.net@gmail.com>
2017-04-19 16:44:28 -0600

    Iniciado ingestor: problemas de codificación de las páginas

--- kapanlagi/template.js
+++ kapanlagi/template.js
@@ -2,7 +2,11 @@
 
 const structure_template = (`
 <section class="post-heading">
-    {{{ post_heading }}}
+    <h1> {{title}} </h1>
+    <div> {{{metadata}}} </div>
+    <div class="main-image">
+        <img data-libingester-asset-id="{{ main_image.asset_id }}">
+    </div>
 </section>
 <section class="post-body">
     {{{ post_body }}}
