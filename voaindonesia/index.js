c418f233459170a978f983146592ee014bcc94c9
Carlos Caal <cammend.net@gmail.com>
2017-05-02 15:35:02 -0600

    Mejoras a voaindonesia

--- voaindonesia/index.js
+++ voaindonesia/index.js
@@ -4,47 +4,83 @@ const libingester = require('libingester');
 const mustache = require('mustache');
 const rss2json = require('rss-to-json');
 const template = require('./template');
+const template_gallery = require('./template_gallery');
 const url = require('url');
 
 const base_uri = 'http://www.voaindonesia.com/';
+
+const attr_image = [
+    'class',
+    'src',
+];
+
+const remove_elements = [
+    '.buttons',
+    '.clear',
+    '.embed-player-only',
+    '.infgraphicsAttach',
+    '.load-more',
+    '.player-and-links',
+];
 
 function ingest_article(hatch, uri) {
     return libingester.util.fetch_html(uri).then(($profile) => {
         const base_uri = libingester.util.get_doc_base_uri($profile, uri);
         const asset = new libingester.NewsArticle();
 
-        //Set title section
+        // set title section
         const title = $profile('meta[property="og:title"]').attr('content');
         asset.set_title(title);
         asset.set_canonical_uri(uri);
 
-        // Pull out the updated date
+        // pull out the updated date
         const date = $profile('div.published').find('time').first();
         const datetime = $profile(date).attr('datetime');
         asset.set_last_modified_date(new Date( Date.parse(datetime) ));
         const section_type = $profile('meta[property="og:type"]').attr('content');
         asset.set_section(section_type);
 
-        // Pull out the main image
+        // pull out the main image
         const url_main_image = $profile('meta[property="og:image"]').attr('content');
         const main_img = libingester.util.download_image(url_main_image);
         const main_img_caption =  $profile('div.image').find('p[itemprop="caption"]').first();
         hatch.save_asset(main_img);
 
         // template data
-        if( $profile('div.intro')[0] ){ // data for 'video post'
-            var section = $profile('div.authors ul li').children();
-            var body = $profile('div.intro').children();
-        }else{                          // data for 'article post'
-            var section = $profile('div.category').children();
-            var body = $profile('div.body-container').find('div.wsw').first();
+        const body_gallery = $profile('div.container').find('div#galleryItems');
+        const body_video = $profile('div.container').find('div.intro').first();
+        const body_post = $profile('div.body-container').find('div.wsw').first();
+        var mustache_template = template.structure_template;
+        var body;
+        var section;
+
+        // body and section article
+        if( body_post[0] ){             // data for 'article post'
+            section = $profile('div.category').children();
+            body = body_post;
+        }else if( body_video[0] ){      // data for 'video post'
+            section = $profile('div.authors ul li').children();
+            body = body_video;
+        }else if( body_gallery[0] ){    // data for 'gallery post'
+            section = $profile('div.category').children();
+            body = body_gallery;
+            mustache_template = template_gallery.structure_template;
         }
-        
+
+        // remove elements
+        for(const element of remove_elements){
+            body.find(element).remove();
+        }
+
         // download images
         body.find('img').map(function(){
             if( this.attribs.src ){
+                this.attribs.src = this.attribs.src.replace('_q10','');
                 const image = libingester.util.download_image( this.attribs.src );
                 this.attribs["data-libingester-asset-id"] = image.asset_id;
+                for(const attr of attr_image){
+                    delete this.attribs[attr];
+                }
                 hatch.save_asset(image);
             }
         });
@@ -72,7 +108,7 @@ function ingest_article(hatch, uri) {
         });
 
         // render template
-        const content = mustache.render(template.structure_template, {
+        const content = mustache.render(mustache_template, {
             title: title,
             section: section.html(),
             date: date.html(),
@@ -89,10 +125,11 @@ function ingest_article(hatch, uri) {
 
 function main() {
     const hatch = new libingester.Hatch();
-    const audio_urls = 'http://www.voaindonesia.com/z/585';
+    const audio_urls = 'http://www.voaindonesia.com/z/585'; //audio posts
     const rss_urls = [
-        'http://www.voaindonesia.com/api/zo-ovegyit',
-        'http://www.voaindonesia.com/api/zrjqpeu_om',
+        'http://www.voaindonesia.com/api/zo-ovegyit',       //video posts
+        'http://www.voaindonesia.com/api/zmgqoe$moi',       //berita posts
+        'http://www.voaindonesia.com/api/zp-oqe-yiq',       //gallery posts
     ];
 
     // getting url's
@@ -121,7 +158,7 @@ function main() {
     // saving articles
     get_urls().then((post_urls) => {
         Promise.all(post_urls.map((uri) => ingest_article(hatch, uri))).then(() => {
-            hatch.finish();
+            return hatch.finish();
         });
     });
 }
