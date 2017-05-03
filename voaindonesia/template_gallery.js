14b85eb2dcca275aa24b7cd36d6dc77eac9f1cd4
Carlos Caal <cammend.net@gmail.com>
2017-05-03 13:05:09 -0600

    Agregado template gallery a voaindonesia

---
+++ voaindonesia/template_gallery.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const structure_template = (`
+<section class="title">
+    <h1>{{ title }}</h1>
+    <div>{{{ section }}} | {{{ date }}}</div>
+</section>
+<section class="body">
+    {{{ body }}}
+</section>
+`);
+
+exports.structure_template = structure_template;
