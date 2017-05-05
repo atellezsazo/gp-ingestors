402d5f83c4713dfc0a6fdabf891aec0421c1ba44
Carlos Caal <cammend.net@gmail.com>
2017-05-05 09:19:53 -0600

    Terminado con css en livingloving

--- livingloving/template.js
+++ livingloving/template.js
@@ -1,15 +1,32 @@
 'use strict';
 
 const structure_template = (`
-<section class="title">
+<style>
+    {{{style}}}
+</style>
+<header>
+    <div class="extra-header">
+        <div class="context">{{{category}}}</div>
+        <div class="extra-header-right">{{author}} &#x2022; <span class="date-published">{{date_published}}</span></div>
+    </div>
     <h1>{{ title }}</h1>
-    <div class="meta">{{{ meta }}}</div>
-</section>
+</header>
 <section class="main-image">
     <img data-libingester-asset-id="{{ main_image.asset_id }}">
+    {{#image_credit}}
+    <div class="image-credit">{{ image_credit }}</div>
+    {{/image_credit}}
 </section>
-<section class="body">
+<main class="main">
     {{{ body }}}
-</section>`);
+    {{#post_tags}}
+    <div class="post-tags">{{{ post_tags }}}</div>
+    {{/post_tags}}
+    <div class="sources">
+        <div class="app-logo"><p>{{app_name}}</p></div>
+        <div class="source"><p class="page-link"><a href="http://www.livingloving.net/">Original Article at www.livingloving.net</a></p></div>
+    </div>
+</main>
+`);
 
 exports.structure_template = structure_template;
