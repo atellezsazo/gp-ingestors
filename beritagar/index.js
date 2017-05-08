e68f2c1e9a0a056b18beee16e1fd25444cde2ae0
Carlos Caal <cammend.net@gmail.com>
2017-05-08 15:14:18 -0600

    Finalizado ingestor beritagar y arreglado el render para diferentes tipos de posts

--- beritagar/index.js
+++ beritagar/index.js
@@ -11,17 +11,43 @@ const url = require('url');
 const rss_uri = 'https://beritagar.id/rss'; //Artists
 const base_uri = 'https://beritagar.id/';
 
-//Remove elements (body)
-const remove_elements = [
-    'div.article-sharer',
-    'div.gallery-list',
-    'div.gallery-navigation',
-    'div.gallery-single',
-    'iframe',
-];
 // clean images
 const remove_attr_img = [
+    'class',
     'data-src',
+    'src',
+    'style',
+];
+
+//Remove elements (body)
+const remove_elements = [
+    '.article-recomended',
+    '.article-sharer',
+    '.article-sub-title',
+    '.follow-bar',
+    '.gallery-list',
+    '.gallery-navigation',
+    '.gallery-single',
+    '.unread',
+    '#commentFBDesktop',
+    '#load-more-btn',
+    '#opinibam',
+    '#semar-placement',
+    '#semar-placement-v2',
+    'blockquote',
+    'iframe',
+    'script',
+];
+
+const remove_elements_header = [
+    '.sponsored-socmed',
+    'iframe',
+    'script',
+];
+
+//embedded content
+const video_iframes = [
+    'youtube', //YouTube
 ];
 
 function ingest_article(hatch, uri) {
@@ -36,67 +62,128 @@ function ingest_article(hatch, uri) {
         asset.set_title(title);
         asset.set_canonical_uri(uri);
 
-        // select body for 'galery' or 'article'
-        if( section === 'GALERI' )
-            var body = $('div.gallery-all').first();
-        else
-            var body = $('div.article-excerpt').first();
-
         // Pull out the updated date and section
         const modified_time = $('meta[property="article:modified_time"]').attr('content');
         asset.set_last_modified_date(new Date(Date.parse(modified_time)));
-        const article_info = $('div.article-info').first();
+        const article_info = $('.article-info');
 
-        // clean body and tag info
-        article_info.find('img').remove();
-        for(const element of remove_elements)
-            body.find(element).remove();
-
-        // download image
-        body.find('img').map(function() {
-            if( this.attribs.src ){
-                //console.log(this.attribs.src);
-                const image = libingester.util.download_image( this.attribs.src );
-                this.attribs["data-libingester-asset-id"] = image.asset_id;
+        // download images
+        const download_image = (that) => {
+            if( that.attribs.src ){
+                const image = libingester.util.download_image( that.attribs.src );
+                that.attribs["data-libingester-asset-id"] = image.asset_id;
+                for(const attr of remove_attr_img){
+                    delete that.attribs[attr];
+                }
                 hatch.save_asset(image);
-                for(const attr of remove_attr_img)
-                    delete this.attribs[attr];
             }
+        }
+
+        // download videos
+        const download_video = (that) => {
+            let video_url = that.attribs.src;
+            for (const video_iframe of video_iframes) {
+                if (video_url.includes(video_iframe)) {
+                    const video_asset = new libingester.VideoAsset();
+                    video_asset.set_canonical_uri(video_url);
+                    video_asset.set_last_modified_date(modified_time);
+                    video_asset.set_title(title);
+                    video_asset.set_download_uri(video_url);
+                    hatch.save_asset(video_asset);
+                }
+            }
+        }
+
+        // data for article
+        const body = $('section.article-content').first(); // body post
+        const author = article_info.find('address').first(); // author post
+        const published = article_info.find('time').first(); // published data
+        const article_bg = $('.article-background-image').first(); // as the main image, appears sometimes
+        const article_tags = $('#main .media-channel').first(); // article tags, appears sometimes
+        const article_header = $('.article-header .breadcrumb').first(); // other article tags, appears sometimes
+        const article_subtitle = $('.article-sub-title').first(); // article subtitle, appears sometimes
+        const media_subtitle = $('.media-sub-title').first(); // media subtitle, appears sometimes (media post)
+
+        // fix relative paths
+        article_tags.find('a').map(function () {
+            this.attribs.href = url.resolve(base_uri, this.attribs.href);
+        });
+        article_header.find('a').map(function () {
+            this.attribs.href = url.resolve(base_uri, this.attribs.href);
+        });
+
+        // download background image
+        let bg_img = false;
+        if( article_bg.length != 0 ) {
+            const bg = article_bg[0].attribs.style;
+            const bg_img_uri = bg.substring(bg.indexOf('http'), bg.indexOf('jpg')+3);
+            bg_img = libingester.util.download_image( bg_img_uri );
+            hatch.save_asset(bg_img);
+        }
+
+        // download background video (video post)
+        let bg_img_video = false;
+        if( body.length == 0 ) {
+            const bg_img_video_uri = $('meta[property="og:image"]').attr('content');
+            bg_img_video = libingester.util.download_image( bg_img_video_uri );
+            hatch.save_asset(bg_img_video);
+            $('#main').find('iframe').map(function() {
+                download_video(this);
+            });
+        }
+
+        // download image (body)
+        body.find('img').map(function() {
+            download_image(this);
+        });
+
+        // download image (author avatar)
+        author.find('img').map(function() {
+            download_image(this);
         });
 
         // download videos
-        const videos = body.find('iframe').map(function() {
-            let video_url = this.attribs.src;
-            video_url = video_url.substring(0, video_url.indexOf('?'));
-            video_url = video_url.replace('embed/','watch?v=');
-            const video_asset = new libingester.VideoAsset();
-            video_asset.set_canonical_uri(video_url);
-            video_asset.set_last_modified_date(modified_time);
-            video_asset.set_title(title);
-            video_asset.set_download_uri(video_url);
-            hatch.save_asset(video_asset);
+        body.find('iframe').map(function() {
+            download_video(this);
         });
+
+        // remove body tags and comments
+        for(const element of remove_elements){
+            body.find(element).remove();
+        }
+        for(const element of remove_elements_header){
+            author.find(element).remove();
+        }
+        body.contents().filter((index, node) => node.type === 'comment').remove();
 
         // render template
         const content = mustache.render(template.structure_template, {
             title: title,
-            article_info: article_info.html(),
+            article_header: article_header,
+            article_tags: article_tags,
+            author: author,
+            published: published,
+            bg_img: bg_img,
+            bg_img_video: bg_img_video,
+            article_subtitle: article_subtitle,
+            media_subtitle: media_subtitle,
             body: body.html(),
         });
 
         asset.set_document(content);
         hatch.save_asset(asset);
     }).catch((err) => {
-        console.log(err);
+        return ingest_article(hatch, uri);
     });
 }
 
 function main() {
     const hatch = new libingester.Hatch();
-
     rss2json.load(rss_uri, function(err, rss){
         const post_urls = rss.items.map((datum) => datum.url);
-        Promise.all(post_urls.map( (uri) => ingest_article(hatch, uri) )).then( () => hatch.finish() );
+        Promise.all(post_urls.map( (uri) => ingest_article(hatch, uri) )).then( () => {
+            return hatch.finish();
+        });
     });
 }
 
