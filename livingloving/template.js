f9fbb3dafa4daf21a267a3a430f0f96bb92278e0
Abner Tellez <atellezsazo@gmail.com>
2017-05-12 16:32:32 -0600

    Ingestor de Living Loving
    Ingestor de Living Loving

--- livingloving/template.js
+++ livingloving/template.js
@@ -1,32 +1,30 @@
 'use strict';
 
 const structure_template = (`
-<style>
-    {{{style}}}
-</style>
-<header>
+<section class="header">
     <div class="extra-header">
         <div class="context">{{{category}}}</div>
-        <div class="extra-header-right">{{author}} &#x2022; <span class="date-published">{{date_published}}</span></div>
+        <div class="extra-header-right">
+            <span class="author">{{author}}</span> 
+            <span class="date-published">{{date_published}}</span>
+        </div>
     </div>
     <h1>{{ title }}</h1>
-</header>
+</section>
 <section class="main-image">
     <img data-libingester-asset-id="{{ main_image.asset_id }}">
     {{#image_credit}}
     <div class="image-credit">{{ image_credit }}</div>
     {{/image_credit}}
 </section>
-<main class="main">
+<section class="body">
     {{{ body }}}
+</section>
+<section class="footer">
     {{#post_tags}}
     <div class="post-tags">{{{ post_tags }}}</div>
     {{/post_tags}}
-    <div class="sources">
-        <div class="app-logo"><p>{{app_name}}</p></div>
-        <div class="source"><p class="page-link"><a href="http://www.livingloving.net/">Original Article at www.livingloving.net</a></p></div>
-    </div>
-</main>
+</section>
 `);
 
 exports.structure_template = structure_template;
