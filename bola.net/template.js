11a2e14c5bcf3b7384133f016bd0a2d7dc25932e
Carlos Caal <cammend.net@gmail.com>
2017-05-22 15:26:54 -0600

    Terminado ingestor Bola.net

---
+++ bola.net/template.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const structure_template = (`
+<header>
+    <div class="extra-header">
+        <div class="context">{{{category}}}</div>
+        <div class="extra-header-right">
+            <span class="author">{{author}}</span>
+            <span class="dot"> • </span>
+            <span class="date-published">{{{published}}}</span>
+        </div>
+    </div>
+    <h1>{{ title }}</h1>
+</header>
+<section class="main-image">
+    <img data-libingester-asset-id="{{ main_image.asset_id }}">
+</section>
+<section class="body">
+    {{{ body }}}
+</section>
+`);
+
+const template_gallery = (`
+<header>
+    <div class="extra-header">
+        <div class="context">{{{category}}}</div>
+        <div class="extra-header-right">
+            <span class="date-published">{{{published}}}</span>
+        </div>
+    </div>
+    <h1>{{ title }}</h1>
+</header>
+<section class="body">
+    {{{ body }}}
+    <div class="gallery">
+    {{#gallery}}
+        <img class="gallery-item" data-libingester-asset-id="{{id}}">
+    {{/gallery}}
+    </div>
+</section>
+`);
+
+exports.structure_template = structure_template;
+exports.template_gallery = template_gallery;
