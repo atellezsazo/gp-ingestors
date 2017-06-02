e2bfdbc91d680bb134e52377adbb9d05974e5128
Carlos Caal Mendoza <cammend.net@gmail.com>
2017-06-02 10:29:05 -0600

    Terminado ingestor Phununews.vn

--- phununews.vn/index.js
+++ phununews.vn/index.js
@@ -3,46 +3,102 @@
 const cheerio = require('cheerio');
 const libingester = require('libingester');
 const mustache = require('mustache');
-const rss2json = require('rss-to-json');
-const template = require('./template');
 const url = require('url');
+const template = require('./template');
 
-const base_uri = 'http://phununews.vn/';
-const max_links = 8; // max links per 'rss'
-const article_links = [
-    'http://phununews.vn/tin-tuc/', // domestic
+const BASE_URI = 'http://phununews.vn/';
+const LINKS_ARTICLE = [
+    'http://phununews.vn/tin-tuc/', // news
+    'http://phununews.vn/giai-tri/', // entertainment
+    'http://phununews.vn/thi-truong/', // market
+    'http://phununews.vn/bat-dong-san/', // real estate
+    'http://phununews.vn/doi-song/', // life
+    'http://phununews.vn/tinh-yeu-hon-nhan/', // love
+    'http://phununews.vn/me-va-be/ba-bau/', // mother
+    'http://phununews.vn/lam-dep/thoi-trang/', // beauty
+    'http://phununews.vn/suc-khoe/giam-can/', // health
+    // video links
+    'http://phununews.vn/video/giai-tri/',
+    'http://phununews.vn/video/suc-khoe/',
+    'http://phununews.vn/video/lam-dep-thoi-trang/',
+    'http://phununews.vn/video/doi-song/',
+    'http://phununews.vn/video/chuyen-la/',
+    'http://phununews.vn/video/cuoi/',
+    'http://phununews.vn/video/day-nau-an/',
+    'http://phununews.vn/video/trang-diem/',
 ];
+const MAX_LINKS = 3; // max links per 'rss'
 
 // cleaning elements
-const clean_elements = ['a', 'div', 'figure', 'h2', 'i', 'p', 'span', 'ul'];
+const CLEAN_ELEMENTS = [
+    'a',
+    'div',
+    'figure',
+    'h2',
+    'i',
+    'p',
+    'span',
+    'ul'
+];
 
 // delete attr (tag)
-const remove_attr = ['class', 'data-field', 'data-original', 'h', 'height', 'id',
-    'itemscope', 'itemprop', 'itemtype', 'photoid', 'rel', 'sizes', 'style',
-    'title', 'type', 'w', 'width',
+const REMOVE_ATTR = [
+    'align',
+    'class',
+    'data-field',
+    'data-original',
+    'h',
+    'height',
+    'id',
+    'itemprop',
+    'itemscope',
+    'itemtype',
+    'photoid',
+    'rel',
+    'sizes',
+    'style',
+    'title',
+    'type',
+    'w',
+    'width',
 ];
 
 // remove elements (body)
-const remove_elements = ['.author', '.clear', '.clearfix', 'h1', '.icon_box',
-    '.gachxanh', '.news-avatar', '.news-info', '.news_keyword', '.tag',
-    '.title_detail', 'div[type="RelatedOneNews"]', 'div[type="VideoStream"]',
-    'iframe', 'noscript', 'script', 'style',
+const REMOVE_ELEMENTS = [
+    '.block_timer_share',
+    '.brecrum_cate',
+    '.orther_top',
+    '.relative_new',
+    '.title_news',
+    '#phan-khuc',
+    'span[onclick="ShowPopupSendMail()"]',
+    'iframe',
+    'noscript',
+    'script',
+    'style',
 ];
 
+// delete duplicated elements in array
+Array.prototype.unique=function(a){
+  return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+});
+
+/** ingest_article
+ * @param {Object} hatch The Hatch object of the Ingester library
+ * @param {Object} item The objec {} with metadata (uri, author, etc)
+ */
 function ingest_article(hatch, uri) {
     return libingester.util.fetch_html(uri).then(($) => {
         const asset = new libingester.NewsArticle();
-        const author = $('p.author').text().split('-')[0] || $('.news-info b').text();
-        const body = $($('.noidung').first()[0] || $('.infomationdetail').first()[0]);
-        const category = $('.submenu_detail .left').first().find('a');
+        const body = $('#box_details_news').first();
+        const category = $('.brecrum_cate').clone();
         const copyright = $('meta[name="copyright"]').attr('content');
         const description = $('meta[property="og:description"]').attr('content');
-        const published = body.find('.time').first().text().replace('-', '') || $('.published-date').text(); // for template
-        const modified_time = $('meta[property="article:modified_time"]').attr('content'); // for asset
-        const keywords = $(body.find('.news_keyword').first()[0] || body.find('.tag').first()[0]).clone();
+        const published = $('.block_timer').first().text().replace(/[\s]{2,}/g,'').replace('|',' | ').replace('G',' G'); // for template
+        const modified_time = $('meta[property="article:modified_time"]').attr('content')+published.split('|')[1]; // for asset
+        const keywords = $('.block_tag').clone();
         const section = $('meta[property="article:section"]').attr('content');
         const title = $('meta[property="og:title"]').attr('content');
-        const uri_main_image = body.find('.news-avatar').first().attr('src');
         const uri_thumb = $('meta[property="og:image"]').attr('content');
 
         // article settings
@@ -53,41 +109,47 @@ function ingest_article(hatch, uri) {
         asset.set_synopsis(description);
         asset.set_title(title);
 
-        // main image
-        const main_image = libingester.util.download_image(uri_main_image || uri_thumb);
-        main_image.set_title(title);
-        asset.set_thumbnail(main_image);
-        hatch.save_asset(main_image);
-
         // remove elements and clean tags
-        const clean_attr = (tag, a = remove_attr) => a.forEach((attr) => $(tag).removeAttr(attr));
+        const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
         const clean_tags = (tags) => tags.get().map((t) => clean_attr(t));
         body.find('.tinlienquan').removeAttr('class'); //
-        body.find(remove_elements.join(',')).remove();
-        clean_tags(body.find(clean_elements.join(',')));
-        clean_tags(category.find(clean_elements.join(',')));
-        body.find('a').get().map((a) => a.attribs.href = url.resolve(base_uri, a.attribs.href));
+        body.find(REMOVE_ELEMENTS.join(',')).remove();
+        clean_tags(body.find(CLEAN_ELEMENTS.join(',')));
+        clean_tags(category.find(CLEAN_ELEMENTS.join(',')));
+        body.find('a').get().map((a) => a.attribs.href = url.resolve(BASE_URI, a.attribs.href));
 
         // generating tags
         const categories = cheerio('<div></div>');
-        category.get().map((a) => {
-            categories.append(cheerio(`<a href="${url.resolve(base_uri,a.attribs.href)}">${a.attribs.title}</a>`));
+        category.find('a').get().map((a) => {
+            categories.append(cheerio(`<a href="${url.resolve(BASE_URI,a.attribs.href)}">${$(a).text()}</a>`));
         });
         const tags = cheerio('<div></div>');
         keywords.find('a').get().map((a) => {
-            tags.append(cheerio(`<a href="${url.resolve(base_uri,a.attribs.href)}">${a.attribs.title}</a>`));
+            tags.append(cheerio(`<a href="${url.resolve(BASE_URI,a.attribs.href)}">${a.attribs.title}</a>`));
         });
 
         // download images
+        let thumb;
         body.find('img').get().map((img) => {
             clean_attr(img);
+            img.attribs.src = url.resolve(BASE_URI, img.attribs.src);
             const image = libingester.util.download_img(img);
             image.set_title(title);
             hatch.save_asset(image);
+            if (!thumb) {
+                asset.set_thumbnail(thumb = image);
+            }
         });
 
+        // download main image
+        let main_image;
+        if (!thumb) {
+            main_image = libingester.util.download_image(uri_thumb);
+            asset.set_thumbnail(main_image);
+            hatch.save_asset(main_image);
+        }
+
         const content = mustache.render(template.structure_template, {
-            author: author,
             body: body.html(),
             category: categories.html(),
             main_image: main_image,
@@ -101,18 +163,22 @@ function ingest_article(hatch, uri) {
     })
 }
 
+/** ingest_video
+ * @param {Object} hatch The Hatch object of the Ingester library
+ * @param {Object} item The objec {} with metadata (uri, author, etc)
+ */
 function ingest_video(hatch, uri) {
     return libingester.util.fetch_html(uri).then(($) => {
         const asset = new libingester.VideoAsset();
-        const body = $('div[itemprop="video"]').first();
-        const copyright = $('meta[name="copyright"]').attr('content');
-        const description = body.find('meta[itemprop="description"]').attr('content');
-        const download_uri = $('.player iframe').first().attr('src');
-        const modified_time = body.find('meta[itemprop="uploadDate"]').attr('content');
-        const title = body.find('meta[itemprop="name"]').attr('content');
-        const uri_thumb = body.find('link[itemprop="thumbnailUrl"]').attr('href');
+        const description = $('meta[name="description"]').attr('content');
+        const dwn = $('.videogular-container').first().parent().attr('data-ng-init');
+        const download_uri = dwn.substring(dwn.indexOf('http'), dwn.indexOf('mp4')+3);
+        const published = $('.block_timer').first().text().replace(/[\s]{2,}/g,'').replace('|',' | ').replace('G',' G');
+        const modified_time = $('meta[property="article:modified_time"]').attr('content')+published.split('|')[1];
+        const title = $('meta[property="og:title"]').attr('content');
+        const uri_thumb = $('meta[property="og:image"]').attr('content');
 
-        // download image
+        // download thumbnail
         const thumb = libingester.util.download_image(uri_thumb);
         thumb.set_title(title);
 
@@ -120,7 +186,6 @@ function ingest_video(hatch, uri) {
         asset.set_canonical_uri(uri);
         asset.set_download_uri(download_uri);
         asset.set_last_modified_date(new Date(Date.parse(modified_time)));
-        asset.set_license(copyright);
         asset.set_synopsis(description);
         asset.set_thumbnail(thumb);
         asset.set_title(title);
@@ -133,15 +198,36 @@ function ingest_video(hatch, uri) {
 
 function main() {
     const hatch = new libingester.Hatch();
+    let links = [];
 
-    const ingest = (uri) => {
-        return libingester.util.fetch_html(uri).then(($) => {
-            const uris = $('.txt_link').get().map(a => url.resolve(base_uri, a.attribs.href));
-            console.log(uris);
-        })
+    function get_links(f) {
+        return Promise.all(
+            LINKS_ARTICLE.map((uri) =>
+                libingester.util.fetch_html(uri).then(($) => {
+                    let promises = [];
+                    const uris = $('.txt_link').get().map(a => url.resolve(BASE_URI, a.attribs.href));
+                    for (let i=0; i<MAX_LINKS; i++) {
+                        const link = uris[i];
+                        if (link) {
+                            links.push(link);
+                        }
+                    }
+                })
+            )
+        ).then(f);
     }
 
-    article_links.map(uri => ingest(uri));
+    get_links(() => {
+        Promise.all(links.unique().map((uri) => {
+            if (uri.includes('/video/')) {
+                return ingest_video(hatch, uri);    // ingest video
+            } else {
+                return ingest_article(hatch, uri);  // ingest article
+            }
+        })).then(() => {
+            return hatch.finish();
+        })
+    });
 }
 
 main();
