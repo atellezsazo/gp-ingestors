7543a2fd277fea18b0136a28f7766e37639f80fe
cammend <cammend.net@gmail.com>
2017-06-06 17:16:38 -0600

    Corrección error kapanlagi

--- kapanlagi/template.js
+++ kapanlagi/template.js
@@ -3,20 +3,24 @@
 const structure_template = (`
 <section class="header">
     <div class="extra-header">
-        <div class="context">{{{category}}}</div>
+        <div class="context">{{category}}</div>
         <div class="extra-header-right">
-            <span class="author">{{author}}</span> 
+            <span class="author">{{author}}</span>
             <span class="date-published">{{date_published}}</span>
         </div>
     </div>
     <h1>{{ title }}</h1>
 </section>
-<section class="main-image">
-    <img data-libingester-asset-id="{{ main_image.asset_id }}">
-    {{#image_credit}}
-    <div class="image-credit">{{ image_credit }}</div>
-    {{/image_credit}}
-</section>
+
+{{#main_image}}
+  <section class="main-image">
+      <img data-libingester-asset-id="{{ main_image.asset_id }}">
+      {{#image_credit}}
+      <div class="image-credit">{{ image_credit }}</div>
+      {{/image_credit}}
+  </section>
+{{/main_image}}
+
 <section class="body">
     {{{ body }}}
 </section>
