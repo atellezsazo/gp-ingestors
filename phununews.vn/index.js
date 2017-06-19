5028b55cc6a63fea9b4fe5f52c0b5ed8e966ad00
Carlos Caal Mendoza <cammend.net@gmail.com>
2017-06-19 12:02:56 -0600

    Terminado phununews 2.0

--- phununews.vn/index.js
+++ phununews.vn/index.js
@@ -18,55 +18,51 @@ const LINKS_BY_CATEGORY = [
     'http://phununews.vn/nau-an/', // cooking
     'http://phununews.vn/nha-dep/', // decoration
     'http://phununews.vn/anh/', // images (gallery)
-    // 'http://phununews.vn/video/suc-khoe/',
-    // 'http://phununews.vn/video/lam-dep-thoi-trang/',
-    // 'http://phununews.vn/video/doi-song/',
-    // 'http://phununews.vn/video/chuyen-la/',
-    // 'http://phununews.vn/video/cuoi/',
-    // 'http://phununews.vn/video/day-nau-an/',
-    // 'http://phununews.vn/video/trang-diem/',
 ];
 
-// max links per page (Average per page: 17 links)
-const MAX_LINKS = 2;
+const CUSTOM_SCSS = `
+$primary-light-color: #9F9F9F;
+$primary-medium-color: #3D3B41;
+$primary-dark-color: #FB417F;
+$accent-light-color: #B61358;
+$accent-dark-color: #A30D4C;
+$background-light-color: #EEEEEE;
+$background-dark-color: #E6E6E6;
 
-// cleaning elements
+$title-font: 'FreeSans', 'Helvetica', 'Arial', sans-serif;
+$body-font: 'Roboto';
+$display-font: 'FreeSans', 'Helvetica', 'Arial', sans-serif;
+$context-font: 'FreeSans', 'Helvetica', 'Arial', sans-serif;
+$support-font: 'FreeSans', 'Helvetica', 'Arial', sans-serif;
+
+@import "_default";
+`;
+
+/** max links per page (Average per page: 17 links) **/
+const MAX_LINKS = 1;
+
+/** cleaning elements **/
 const CLEAN_ELEMENTS = [
     'a',
-    'div',
-    'figure',
     'h2',
     'i',
     'p',
     'span',
-    'ul'
+    'ul',
 ];
 
-// delete attr (tag)
+/** delete attr (tag) **/
 const REMOVE_ATTR = [
     'align',
+    'border',
     'class',
-    'data-field',
-    'data-original',
-    'h',
-    'height',
-    'id',
-    'itemprop',
-    'itemscope',
-    'itemtype',
-    'photoid',
-    'rel',
-    'sizes',
-    'style',
-    'title',
-    'type',
-    'w',
-    'width',
     'onclick',
     'onmouseover',
+    'style',
+    'title',
 ];
 
-// remove elements (body)
+/** remove elements (body) **/
 const REMOVE_ELEMENTS = [
     'div',
     'noscript',
@@ -74,32 +70,33 @@ const REMOVE_ELEMENTS = [
     'style',
 ];
 
-// delete duplicated elements in array
+/** delete duplicated elements in array **/
 Array.prototype.unique=function(a){
     return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
 });
 
-// get articles metadata
+/** get articles metadata **/
 function _get_ingest_settings($) {
     const canonical_uri = $('meta[property="og:url"]').attr('content');
     const date = $('meta[property="article:modified_time"]').attr('content');
     return {
         author: $('meta[property="og:site_name"]').attr('content'), // no authors
-        body: $('#content-subject-detail').first(),
+        body: $('#content-subject-detail, .fck_detail').first(),
         canonical_uri: canonical_uri,
         date_published: Date.now(date),
         lede: $(`<p>${$('.short_intro').text()}</p>`),
         modified_date: new Date(Date.parse(date)),
-        // custom_scss: CUSTOM_SCSS,
+        custom_scss: CUSTOM_SCSS,
         read_more: `Baca lebih lanjut tentang <a href="${canonical_uri}">phununews.vn</a>`,
         section: $('meta[property="article:section"]').attr('content') || $('.color1').text(),
         synopsis: $('meta[name="description"]').attr('content'),
         source: 'phununews.vn',
-        title: $('meta[property="og:title"]').attr('content'),
+        title: $('meta[property="og:title"]').attr('content')
+                || $('.title_news h1').first().text(),
     }
 }
 
-// set articles metadata
+/** set articles metadata **/
 function _set_ingest_settings(asset, meta) {
     if (meta.author) asset.set_authors(meta.author);
     if (meta.body) asset.set_body(meta.body)
@@ -119,29 +116,29 @@ function _set_ingest_settings(asset, meta) {
 function _download_image($, meta, hatch, asset) {
     let thumbnail;
     const clean_attr = (tag) => REMOVE_ATTR.forEach(attr => $(tag).removeAttr(attr));
-    // const download = (elem) => {
-    //     if (elem.attribs.src) {
-    //         clean_attr(elem);
-    //         const image = libingester.util.download_img($(elem));
-    //         image.set_title(meta.title);
-    //         hatch.save_asset(image);
-    //         if (!thumbnail) asset.set_thumbnail(thumbnail = image);
-    //     }
-    // }
+
     meta.body.find('img').map((i, elem) => {
         if (elem.attribs.src) {
             // find the parent's for the replace it by tag figure
+            clean_attr(elem);
             let parent = $(elem).parent();
             let img = $(elem).clone();
-            let name;
-            while (parent[0]) { console.log(parent[0].name);
+            let description = '',
+                name;
+
+            // delete wrappers
+            while (parent[0]) {
                 if (parent[0].name == 'p' || parent[0].name == 'table') {
                     name = parent[0].name;
                     parent[0].name = 'figure';
+
+                    delete parent[0].attribs;
+                    if (name == 'table') description = parent.find('tr').last().text();
+
                     parent.children().remove();
                     parent.append(img);
                     break;
-                } else if(parent[0].attribs.id == 'content-subject-detail'){
+                } else if (parent[0].name == 'div') {
                     break;
                 } else {
                     parent = parent.parent();
@@ -149,67 +146,40 @@ function _download_image($, meta, hatch, asset) {
             }
 
             // download image
-            clean_attr(elem);
             const image = libingester.util.download_img(parent.children());
             image.set_title(meta.title);
             hatch.save_asset(image);
             if (!thumbnail) asset.set_thumbnail(thumbnail = image);
-            
+
             // find figcation for this image
-
-                let figcaption = parent.next();
-                parent.append($(`<figcaption>${figcaption.text()}</figcaption>`));
-                figcaption.remove();
-            } else if (name == 'table'){
-
+            if (description.trim() != '') {
+                parent.append($(`<figcaption><p>${description}</p></figcaption>`));
+            } else {
+                const figcaption = parent.next();
+                const em = figcaption.find('em').first()[0]; // tag 'em' is the description
+                const text = $(em).text();
+                if (text.trim() != '') {
+                    parent.append($(`<figcaption><p>${text}</p></figcaption>`));
+                    figcaption.remove();
+                }
             }
         } else {
             $(elem).remove();
         }
-        // if ($(elem).parent()[0].name == 'figure') {
-        //     download(elem);
-        // } else {
-        //     let $tag = $(elem);
-        //     while ($tag.parent()[0]) {
-        //         if ($tag.parent()[0].name != 'figure') {
-        //             $tag = $tag.parent();
-        //         } else {
-        //             $tag.replaceWith($(elem));
-        //         }
-        //     }
-        //     download(elem);
-        // }
     });
     return thumbnail;
 }
 
 /** ingest_article
  * @param {Object} hatch The Hatch object of the Ingester library
- * @param {Object} item The objec {} with metadata (uri, author, etc)
+ * @param {String} uri The post uri
  */
 function ingest_article(hatch, uri) {
     return libingester.util.fetch_html(uri).then(($) => {
         const asset = new libingester.NewsArticle();
         let meta = _get_ingest_settings($);
-        // // const body = $('#box_details_news').first();
-        // const category = $('.brecrum_cate').clone();
-        // // const copyright = $('meta[name="copyright"]').attr('content');
-        // const description = $('meta[property="og:description"]').attr('content');
-        // const published = $('.block_timer').first().text().replace(/[\s]{2,}/g,'').replace('|',' | ').replace('G',' G'); // for template
-        // const modified_time = $('meta[property="article:modified_time"]').attr('content')+published.split('|')[1]; // for asset
-        // const keywords = $('.block_tag').clone();
-        // const section = $('meta[property="article:section"]').attr('content');
-        // const title = $('meta[property="og:title"]').attr('content');
         const uri_thumb = $('meta[property="og:image"]').attr('content');
-
-        // article settings
         console.log('processing',meta.title);
-        // asset.set_canonical_uri(uri);
-        // asset.set_last_modified_date(new Date(Date.parse(modified_time)));
-        // asset.set_license(copyright);
-        // asset.set_section(section);
-        // asset.set_synopsis(description);
-        // asset.set_title(title);
 
         // remove elements and clean tags
         const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
@@ -219,26 +189,8 @@ function ingest_article(hatch, uri) {
         clean_tags(meta.body.find(CLEAN_ELEMENTS.join(',')));
         meta.body.find('a').get().map((a) => a.attribs.href = url.resolve(BASE_URI, a.attribs.href));
 
-        // generating tags
-        // const categories = $('<div></div>');
-        // category.find('a').get().map((a) => {
-        //     categories.append($(`<a href="${url.resolve(BASE_URI,a.attribs.href)}">${$(a).text()}</a>`));
-        // });
-        // const tags = $('<div></div>');
-        // keywords.find('a').get().map((a) => {
-        //     tags.append($(`<a href="${url.resolve(BASE_URI,a.attribs.href)}">${a.attribs.title}</a>`));
-        // });
-
         // download images
         let thumbnail = _download_image($, meta, hatch, asset);
-        // meta.body.find('img').get().map(img => {
-        //     clean_attr(img);
-        //     img.attribs.src = url.resolve(BASE_URI, img.attribs.src);
-        //     const image = libingester.util.download_img(img);
-        //     image.set_title(meta.title);
-        //     hatch.save_asset(image);
-        //     if (!thumbnail) asset.set_thumbnail(thumbnail = image);
-        // });
 
         // download main image
         if (!thumbnail) {
@@ -248,17 +200,65 @@ function ingest_article(hatch, uri) {
             asset.set_main_image(image, '');
             hatch.save_asset(image);
         }
-        meta.body.find('p').filter((i,p) => $(p).text().trim() === '').remove();
+
+        meta.body.find('p,figcation').filter((i,elem) => $(elem).text().trim() === '').remove();
 
         _set_ingest_settings(asset, meta);
         asset.render();
         hatch.save_asset(asset);
-    }).catch(err => console.log(err));
+    }).catch(err => { console.log(err);
+        if (err.code == 'ECONNRESET') return ingest_article(hatch, item);
+    });
 }
 
 /** ingest_video
  * @param {Object} hatch The Hatch object of the Ingester library
- * @param {Object} item The objec {} with metadata (uri, author, etc)
+ * @param {String} uri The post uri
+ */
+function ingest_gallery(hatch, uri) {
+    return libingester.util.fetch_html(uri).then($ => {
+        const asset = new libingester.NewsArticle();
+        let meta = _get_ingest_settings($);
+        console.log('processing',meta.title);
+
+        // remove elements and clean tags
+        const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
+        const clean_tags = (tags) => tags.get().map((t) => clean_attr(t));
+        meta.body.find(REMOVE_ELEMENTS.join(',')).remove();
+        clean_tags(meta.body.find(CLEAN_ELEMENTS.join(',')));
+
+        // download images
+        let thumbnail;
+        meta.body.find('img').map((i,elem) => {
+            let p = $(elem).parent();
+            while (p[0]) {
+                if (p[0].name != 'p') {
+                    p = p.parent();
+                } else {
+                    break;
+                }
+            }
+            const img = $(`<img src="${elem.attribs.src}" alt="${elem.attribs.alt}"/>`);
+            const caption = $(`<figcaption><p>${elem.attribs.title}</p></figcaption>`);
+            const figure = $(`<figure></figure>`).append(img, caption);
+            const image = libingester.util.download_img($(figure.children()[0]));
+            image.set_title(meta.title);
+            hatch.save_asset(image);
+            if(!thumbnail) asset.set_thumbnail(thumbnail = image);
+            p.replaceWith(figure);
+        });
+
+        _set_ingest_settings(asset, meta);
+        asset.render();
+        hatch.save_asset(asset);
+    }).catch(err => { console.log(err);
+        if (err.code == 'ECONNRESET') return ingest_article(hatch, item);
+    });
+}
+
+/** ingest_video
+ * @param {Object} hatch The Hatch object of the Ingester library
+ * @param {String} uri The post uri
  */
 function ingest_video(hatch, uri) {
     return libingester.util.fetch_html(uri).then($ => {
@@ -270,8 +270,8 @@ function ingest_video(hatch, uri) {
         const modified_time = $('meta[property="article:modified_time"]').attr('content')+published.split('|')[1];
         const title = $('meta[property="og:title"]').attr('content');
         const uri_thumb = $('meta[property="og:image"]').attr('content');
-
         console.log('processing',title);
+
         // download thumbnail
         const thumb = libingester.util.download_image(uri_thumb);
         thumb.set_title(title);
@@ -287,7 +287,9 @@ function ingest_video(hatch, uri) {
         //save assets
         hatch.save_asset(thumb);
         hatch.save_asset(asset);
-    })
+    }).catch(err => { console.log(err);
+        if (err.code == 'ECONNRESET') return ingest_article(hatch, item);
+    });
 }
 
 /** _fetch_all_links (Promise)
@@ -310,28 +312,27 @@ function _fetch_all_links(links, max) {
 }
 
 /** _ingest_by_category (return Promise)
- * @param {Object} hatch The list of links
+ * @param {Object} hatch The Hatch object of the Ingester library
  * @param {String} uri The max number of links per page
  */
 function _ingest_by_category(hatch, uri) {
-    const category = uri.replace(BASE_URI,'');
+    let category = uri.replace(BASE_URI,'');
+    category = category.substring(0, category.indexOf('/')+1);
+
     switch (category) {
         case 'video/': return ingest_video(hatch, uri);
+        case 'anh/': return ingest_gallery(hatch, uri);
         default: return ingest_article(hatch, uri);
     }
 }
 
-
 function main() {
     const hatch = new libingester.Hatch('phununews', 'vi');
 
-    // _fetch_all_links(LINKS_BY_CATEGORY, MAX_LINKS).then(links => {
-    //     Promise.all(links.map(uri => _ingest_by_category(hatch, uri)))
-    //         .then(() => hatch.finish());
-    // });
-    const uri = 'http://phununews.vn/tinh-yeu-hon-nhan/cai-ket-bat-ngo-co-gai-thue-doi-xe-om-gia-nha-trai-sau-khi-bi-ban-trai-huy-hon-188833/';
-    ingest_article(hatch, uri)
-    .then(() => hatch.finish());
+    _fetch_all_links(LINKS_BY_CATEGORY, MAX_LINKS).then(links => {
+        Promise.all(links.map(uri => _ingest_by_category(hatch, uri)))
+            .then(() => hatch.finish());
+    });
 }
 
 main();
