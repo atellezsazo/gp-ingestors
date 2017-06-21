571f47aeab26ca9d16fe83b97df3a0c52c06503b
Carlos Caal Mendoza <cammend.net@gmail.com>
2017-06-21 12:43:40 -0600

    Últimos cambios pergidulu 2.0

--- pergidulu/index.js
+++ pergidulu/index.js
@@ -3,19 +3,21 @@
 const libingester = require('libingester');
 const rss2json = require('rss-to-json');
 
-const BASE_URI = 'https://www.pergidulu.com/';
-const RSS_FEED = 'https://www.pergidulu.com/feed/'; //Artists
+const RSS_FEED = 'https://www.pergidulu.com/feed/';
 
 //Remove attributes (images)
 const REMOVE_ATTR = [
     'class',
+    'data-color-override',
+    'data-hover-color-override',
+    'data-hover-text-color-override',
     'data-lazy-sizes',
     'data-lazy-src',
     'data-lazy-srcset',
     'height',
     'sizes',
-    'src',
     'srcset',
+    'style',
     'width',
 ];
 
@@ -48,52 +50,61 @@ $display-font-composite: 'Roboto';
 @import "_default";
 `;
 
-function ingest_article(hatch, uri) {
-    return libingester.util.fetch_html(uri).then($ => {
+function ingest_article(hatch, obj) {
+    return libingester.util.fetch_html(obj.uri).then($ => {
         const asset = new libingester.BlogArticle();
         const info_article = $('div#single-below-header').first();
-        const author = $(info_article).find('span.fn').text();
+        const author = $(info_article).find('a[rel="author"]').text();
         const body = $('div.post-content div.content-inner').first();
         const canonical_uri = $('link[rel="canonical"]').attr('href');
         const description = $('meta[property="og:description"]').attr('content');
-        const date = $(info_article).find('span.date').text();
         const modified_date = $('meta[property="article:published_time"]').attr('content');
-        const section = $('meta[property="article:section"]').attr('content');
+        const date = new Date(Date.parse(modified_date));
+        const section =$('meta[property="article:section"]').attr('content');
         const read_more = 'Original Article at www.pergidulu.com';
         const title = $('.entry-title').text() || $('meta[property="og:title"]').attr('content');
-        const tags = $('meta[property="article:tag"]').map((i, elem) => elem.attribs.content).get();
+        const tags  = obj.tags.split(' ');
+        const thumb_url = $('meta[property="og:image"]').attr('content');
 
         // Pull out the main image
-        let main_img = $('.post-featured-img img').first();
-        if (typeof main_img.attr('data-lazy-src') !== undefined) {
-            main_img.attr('src', main_img.attr('data-lazy-src'));
+        if (thumb_url) {
+            const main_image = libingester.util.download_image(thumb_url);
+            main_image.set_title(title);
+            hatch.save_asset(main_image);
+            asset.set_thumbnail(main_image);
+            asset.set_main_image(main_image);
         }
-        const main_image = libingester.util.download_img(main_img, BASE_URI);
-        main_image.set_title(title);
-        hatch.save_asset(main_image);
-        asset.set_thumbnail(main_image);
 
         // remove elements and comments
+        const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach(attr => $(tag).removeAttr(attr));
         body.contents().filter((index, node) => node.type === 'comment').remove();
         body.find(REMOVE_ELEMENTS.join(',')).remove();
 
         // download images
         body.find('img').map(function() {
-            if (typeof this.attribs['data-lazy-src'] !== undefined) {
-                this.attribs.src = this.attribs['data-lazy-src'];
+            const lazy_src = this.attribs['data-lazy-src'] || '';
+            if (lazy_src.includes('http')) {
+                this.attribs.src = lazy_src;
+                clean_attr(this);
+                const figure = $('<figure></figure>').append($(this).clone());
+                const image = libingester.util.download_img(figure.children());
+                const parent = $(this).parent();
+                if (parent[0].name == 'p') {
+                    figure.insertAfter(parent);
+                    $(this).remove();
+                } else {
+                    $(this).replaceWith(figure);
+                }
+                image.set_title(title);
+                hatch.save_asset(image);
+            } else {
+                $(this).remove();
             }
-            let img = $('<figure></figure>').append($(this).clone());
-            const image = libingester.util.download_img(img.children());
-            $(this).replaceWith(img);
-            image.set_title(title);
-            hatch.save_asset(image);
-            this.attribs["data-libingester-asset-id"] = image.asset_id;
         });
 
         //clean tags
-        const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
-        body.find("img").get().map((tag) => clean_attr(tag));
-        body.find('p').filter((i, elem) => $(elem).text().trim() === '').remove();
+        body.find('img, p, span').get().map(tag => clean_attr(tag));
+        body.find('p').filter((i,elem) => $(elem).text().trim() === '').remove();
 
         console.log('processing', title);
         asset.set_author(author);
@@ -101,8 +112,8 @@ function ingest_article(hatch, uri) {
         asset.set_canonical_uri(canonical_uri);
         asset.set_title(title);
         asset.set_synopsis(description);
-        asset.set_date_published(modified_date);
-        asset.set_last_modified_date(new Date(Date.parse(modified_date)));
+        asset.set_date_published(Date.now(date));
+        asset.set_last_modified_date(date);
         asset.set_read_more_text(read_more);
         asset.set_tags(tags);
         asset.set_custom_scss(CUSTOM_CSS);
@@ -112,13 +123,18 @@ function ingest_article(hatch, uri) {
 }
 
 function main() {
-    const hatch = new libingester.Hatch('pergidulu', {
-        argv: process.argv.slice(2)
-    });
-    rss2json.load(RSS_FEED, (err, rss) => {
-        if (err) throw { code: -1, message: 'Error to load rss' }
-        const links = rss.items.map(item => item.url);
-        Promise.all(links.map(uri => ingest_article(hatch, uri)))
+    const hatch = new libingester.Hatch('pergidulu', 'id');
+
+    libingester.util.fetch_html(RSS_FEED).then($ => {
+        const items = $('item').map((i,item) => {
+            const contents = $(item).contents().get();
+            return {
+                uri: contents[4].data.replace(/[\n\t]/g,''),
+                tags: contents[11].children[0].data.replace('[CDATA[','').replace(']]',''),
+            }
+        }).get();
+        
+        Promise.all(items.map(item => ingest_article(hatch, item)))
             .then(() => hatch.finish());
     });
 }
