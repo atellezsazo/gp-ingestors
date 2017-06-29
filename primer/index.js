01d0350963d545e4a4d6621aed31a7a7ebd06f1d
Andres De la Cruz <delacruzjagp360@gmail.com>
2017-06-29 12:40:39 -0600

    T17411/Primer-libingester2.0

--- primer/index.js
+++ primer/index.js
@@ -6,20 +6,22 @@ const rss2json = require('rss-to-json');
 const url = require('url');
 const xml2js = require('xml2js');
 
-const BASE_URI = 'https://www.kapanlagi.com/';
-const MAX_ATTEMPTS  = 3;
-const MAX_LINKS = 60;
-const RSS_URI = 'https://www.kapanlagi.com/feed/';
+const BASE_URI = 'http://primer.com.ph/blog/2017/';
+const RSS_URI = 'http://primer.com.ph/blog/feed/';
 
 // Remove elements (body)
 const REMOVE_ELEMENTS = [
-    'b',
-    'iframe',
+    'div',
     'h2',
     'h6',
+    'hr',
+    'iframe',
     'noscript',
     'script',
     'style',
+    '.cat-meta',
+    '.cat-header',
+    '.single-siteurl',
     '.lifestyle-in-content',
     '.link-pagging-warper',
     '.paging-related',
@@ -53,183 +55,136 @@ const CLEAN_TAGS = [
 ];
 
 const CUSTOM_CSS = `
-$primary-light-color: #EE3A81;
-$primary-medium-color: #336699;
-$primary-dark-color: #100F0F;
-$accent-light-color: #F2B328;
-$accent-dark-color: #F68B29;
-$background-light-color: #EDEDED;
-$background-dark-color: #E3E3E3;
+$primary-light-color: #06B0EF;
+$primary-medium-color: #056F96;
+$primary-dark-color: #3A3A3A;
+$accent-light-color: #FAD213;
+$accent-dark-color: #E14163;
+$background-light-color: #FAFAFA;
+$background-dark-color: #EBEBEB;
+
 $title-font: 'Lato';
-$body-font: 'Merriweather';
-$display-font: 'Oswald';
-$context-font: 'Oswald';
+$body-font: 'Lato';
+$display-font: 'Lato';
+$logo-font: 'Lato';
+$context-font: 'Lato';
 $support-font: 'Lato';
 
 @import '_default';
 `;
 
-function ingest_article(hatch, uri) {
-    return libingester.util.fetch_html(uri).then(($) => {
-        if ($('meta[http-equiv="REFRESH"]').length == 1) throw { name: 'Article have a redirect' };
-        if ($('title').text().includes('404')) throw { name: 'Not Found 404' };
+function ingest_article(hatch, item) {
+    return libingester.util.fetch_html(item.link).then(($) => {
 
-        const asset = new libingester.NewsArticle();
-        const category = $(".newsdetail-categorylink").first().text();
+        const asset = new libingester.BlogArticle();
+        const body = $('.single-content').first();
         const canonical_uri = $('link[rel="canonical"]').attr('href');
-        const date = $('.vcard .newsdetail-schedule-new.updated').text();
-        const info_date = $('.newsdetail-schedule-new .value-title').attr('title');
-        const modified_date = info_date ? new Date(Date.parse(info_date)) : new Date();
-        const post_tags = $('.box-content a');
-        const page = 'kapanlagi';
-        const read_more = `Read more at <a href="${canonical_uri}">${page}</a>`;
-        const reporter = $('.vcard .newsdetail-schedule-new a').text();
-        const subtitle = $("h2.entertainment-newsdetail-title-new").first().text();
-        const synopsis = $('meta[name="description"]').attr('content');
-        const section = $('meta[name="adx:sections"]').attr('content');
-        const title = $("#newsdetail-right-new h1").first().text();
+        const description = $('meta[property="og:description"]').attr('content');
+        const modified_date = new Date(item.pubdate);
+        const section ='Article';
+        const page = 'Primer';
+        const read_more = 'Read more at www.primer.com.ph';
+        const title = $('meta[property="og:title"]').attr('content');
         const uri_main_image = $('meta[property="og:image"]').attr('content');
+        const tags = item.categories;
 
         // Pull out the main image
-        let main_image, image_credit;
         if (uri_main_image) {
-            main_image = libingester.util.download_image(uri_main_image, uri);
-            main_image.set_title(title);
-            image_credit = $('.entertainment-newsdetail-headlineimg .copyright, .pg-img-warper span').text();
-            hatch.save_asset(main_image);
-            asset.set_thumbnail(main_image);
+            const main_img = libingester.util.download_image(uri_main_image);
+            main_img.set_title(title);
+            hatch.save_asset(main_img);
+            asset.set_thumbnail(main_img);
         }
 
-        const body_page = $('<div></div>');
-        const ingest_body = ($, finish_process) => {
-            const body = $('.entertainment-detail-news');
-            const next = $('.link-pagging-warper a').attr('href');
-            const last_pagination = $('ul.pg-pagging li:last-child a').first();
-
-            // resolve links
-            body.find("a").map((i, elem) => {
-                if($(elem).attr('href'))
-                    $(elem).attr('href',url.resolve(BASE_URI,$(elem).attr('href')));
-            });
-
-            // set first paragraph
-            const first_p = body.find('p').first();
-            const lede = first_p.clone();
-            lede.find('img').remove();
-            asset.set_lede(lede);
-            body.find(first_p).remove();
-
-            // remove elements and comments
-            const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
-            body.contents().filter((index, node) => node.type === 'comment').remove();
-            body.find(REMOVE_ELEMENTS.join(',')).remove();
-            body.find(CLEAN_TAGS.join(',')).get().map((tag) => clean_attr(tag));
-            body.find('p').filter((i,elem) => $(elem).text().trim() === '').remove();
-            let editor = body.find('span').last();
-
-            if(editor.text().includes('Editor:')){
-                editor.parent().remove()
+        body.find('p img').map(function(){
+            const span=$(this).next()[0] || {};
+            if(span.name=='span'){
+                $(span).insertAfter($(this).parent());
             }
+            $(this).insertAfter($(this).parent());
 
-            // Download images
-            body.find("p img").map((i, elem)=> {
-                const parent=$(elem).parent();
-                if (elem.attribs.src) {
-                    if(elem.attribs.alt){
-                        var figcaption = $("<figcaption><p>"+elem.attribs.alt.replace('�',' - ')+"</p></figcaption>");
-                        let img = $('<figure></figure>').append($(elem).clone(),figcaption);
-                    }
-                    else {
-                        let img = $('<figure></figure>').append($(elem).clone());
-                    }
-                    const image = libingester.util.download_img($(img.children()[0]));
-                    if(parent[0].name == 'div'){
-                        parent.replaceWith(img);
-                    }
-                    else{
-                        $(elem).replaceWith(img);
-                    }
-                    image.set_title(title);
-                    hatch.save_asset(image);
-                    elem.attribs["data-libingester-asset-id"] = image.asset_id;
-                } else {
-                    $(elem).remove();
-                }
-            });
+        });
 
-            body_page.append(body.children());
-            if (next && last_pagination.length != 0) {
-                libingester.util.fetch_html(url.resolve(uri, next)).then(($next_profile) => {
-                    ingest_body($next_profile, finish_process);
-                });
-            } else {
-                finish_process();
-            }
-        };
-
-        return new Promise((resolve, reject) => {
-            ingest_body($, () => {
-
-                console.log('processing', title);
-                asset.set_authors([reporter]);
-                asset.set_canonical_uri(canonical_uri);
-                asset.set_canonical_uri(uri);
-                asset.set_custom_scss(CUSTOM_CSS);
-                asset.set_date_published(Date.now(modified_date));
-                asset.set_last_modified_date(modified_date);
-                asset.set_read_more_link(read_more);
-                asset.set_section(section);
-                asset.set_source(page);
-                asset.set_synopsis(synopsis);
-                asset.set_title(title);
-                asset.set_main_image(main_image,image_credit);
-                asset.set_body(body_page);
-
-                asset.render();
-                hatch.save_asset(asset);
-                resolve();
-            });
-        })
-    }).catch((err) => {
-        console.log("Ingest error: ", err);
-    });
-}
-
-function main() {
-    const hatch = new libingester.Hatch('kapanlagi', 'id');
-
-    let attempt = 1;
-
-    const __request = (f) => {
-        rp({ uri: RSS_URI, gzip: true }).then(res => {
-            var parser = new xml2js.Parser({ trim: false, normalize: true, mergeAttrs: true });
-            parser.parseString(res, (err, result) => {
-                const rss = rss2json.parser(result);
-                let links = [],
-                    n = 0;
-                const items=rss.items.slice(MAX_LINKS);
-                for(const item of items){
-                    if (!item.link.includes("musik.kapanlagi.com")) { //drop musik subdomain
-                        links.push(item.link);
-                    }
-                }
-                f(links); //callback
-            });
-        }).catch(err => {
-            console.log('Error load Rss:', err);
-            if (attempt++ < MAX_ATTEMPTS) {
-                __request(f);
+        // download video
+        body.find('iframe').map(function() {
+            const src = this.attribs.src;
+            if (src.includes("youtube")) {
+                const video = libingester.util.get_embedded_video_asset($(this), src);
+                video.set_title(title);
+                hatch.save_asset(video);
             }
         });
+
+        //download facebook video
+        body.find('#fb-root').map(function(){
+            let video_title= $(this).prev();
+            let fb_video=$(this).next().next().attr('data-href');
+            video_title.replaceWith($(`<a href="${fb_video}">${video_title.text()}</a>`));
+        });
+
+        //clean tags
+        const clean_attr = (tag, a = REMOVE_ATTR) => a.forEach((attr) => $(tag).removeAttr(attr));
+        body.find(REMOVE_ELEMENTS.join(',')).remove();
+        body.find(CLEAN_TAGS.join(',')).get().map((tag) => clean_attr(tag));
+        body.find('p').filter((i,elem) => $(elem).is(':empty')).remove(); //Cleaning empty paragraph
+
+        let author = $('meta[name="author"]').attr('content');
+        let info_trash = body.find('p').last()[0];
+        if (info_trash) {
+            const text = $(info_trash).find('em').first().text();
+            if (text.includes('Written by:')) {
+                author=text.replace('Written by:','');
+            }
+            $(info_trash).remove();
+        }
+
+       // download images
+       body.find('img').map(function() {
+           let img = $('<figure></figure>').append($(this).clone());
+           let figcaption = $(this).next()[0] || {};
+           if (figcaption.name == 'span') {
+               img.append($(`<figcaption><p>${$(figcaption).text()}</p></figcaption>`));
+               $(figcaption).remove();
+           }
+           const image = libingester.util.download_img($(img.children()[0]));
+           $(this).replaceWith(img);
+           image.set_title(title);
+           hatch.save_asset(image);
+       });
+
+        // Article Settings
+        console.log('processing', title);
+        asset.set_author(author);
+        asset.set_body(body);
+        asset.set_canonical_uri(canonical_uri);
+        asset.set_title(title);
+        asset.set_synopsis(description);
+        asset.set_date_published(modified_date);
+        asset.set_last_modified_date(new Date(Date.parse(modified_date)));
+        asset.set_read_more_text(read_more);
+        asset.set_tags(tags);
+        asset.set_custom_scss(CUSTOM_CSS);
+
+        asset.render();
+        hatch.save_asset(asset);
+    }).catch((err) => {
+        if (err.code == -1 || err.statusCode == 403) {
+            return ingest_article(hatch, uri);
+        }
+    });
     }
 
-    __request((links) => {
-        console.log(links);
+function main() {
 
-        Promise.all(links.map((link) => ingest_article(hatch, link))).then(() => {
-            return hatch.finish();
-        });
-    })
+    // wordpress pagination
+    const feed = libingester.util.create_wordpress_paginator(RSS_URI);
+    const hatch = new libingester.Hatch('primer', 'en');
+    libingester.util.fetch_rss_entries(feed, 20, 100).then(rss => {
+             Promise.all(rss.map(item => ingest_article(hatch, item)))
+                     .then(() => hatch.finish());
+        }).catch((err) => {
+            console.log('Error '+err);
+         });
 }
 
 main();
