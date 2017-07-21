1e5e56fd6eaf9e94ab069509e24e3e1d2ba60d5c
Andres De la Cruz <delacruzjagp360@gmail.com>
2017-07-21 14:45:16 -0600

    Content ingestion

--- alo_dokter/index.js
+++ alo_dokter/index.js
@@ -20,18 +20,20 @@ Array.prototype.unique = function(a) {
 const MAX_LINKS = 10;
 
 const CUSTOM_CSS = `
-$primary-light-color: #E50200;
-$primary-medium-color: #262626;
-$primary-dark-color: #000000;
-$accent-light-color: #E50200;
-$accent-dark-color: #C90200;
-$background-light-color: #F4F4F4;
-$background-dark-color: #CCCCCC;
-$title-font: 'Roboto';
-$body-font: 'Merriweather';
-$display-font: 'Titillium Web';
-$context-font: 'Titillium Web';
-$support-font: 'Roboto';
+$primary-light-color: #FF4C00;
+$primary-medium-color: #36332F;
+$primary-dark-color: #5F5F5F;
+$accent-light-color: #1D70D9;
+$accent-dark-color: #1161C7;
+$background-light-color: #F6F4F1;
+$background-dark-color: #C9C3BD;
+
+$title-font: 'Raleway';
+$body-font: 'Roboto';
+$display-font: 'Raleway';
+$context-font: 'Raleway';
+$support-font: 'Raleway';
+
 @import "_default";
 `;
 
@@ -61,7 +63,6 @@ const REMOVE_ELEMENTS = [
     '.share-button',
 ];
 
-
 function ingest_article(hatch, uri) {
     return libingester.util.fetch_html(uri).then($ => {
         const asset = new libingester.NewsArticle();
@@ -90,20 +91,16 @@ function ingest_article(hatch, uri) {
         body.find(first_p).remove();
 
         // download images
-        body.find('img').map(function() {
-            let img = $('<figure></figure>').append($(this).clone());
-            let figcaption = $(this).attr('alt') || {};
-            img.append($(`<figcaption><p>${figcaption}</p></figcaption>`));
-            $(figcaption).remove();
+        body.find('img').map((i,elem) => {
+            let img = $('<figure></figure>').append($(elem).clone());
             const image = libingester.util.download_img($(img.children()[0]));
             if (!thumbnail) {
                 asset.set_thumbnail(thumbnail=image);
             }
-            $(this).replaceWith(img);
+            $(elem).replaceWith(img);
             image.set_title(title);
             hatch.save_asset(image);
        });
-
 
         // clean tags
         body.find(CLEAN_TAGS.join(',')).map((i,elem) => clean_attr(elem));
@@ -128,8 +125,14 @@ function ingest_article(hatch, uri) {
             }
         });
 
+        body.find('p>em').map((i,elem) => {
+            if ($(elem).text().includes('sponsored by:')) {
+                $(elem).parent().remove()
+            }
+        });
+
         // Article Settings
-        console.log('processing: ', title);
+        console.log('processing: ', uri);
         asset.set_authors([author]);
         asset.set_canonical_uri(canonical_uri);
         asset.set_custom_scss(CUSTOM_CSS);
@@ -151,97 +154,6 @@ function ingest_article(hatch, uri) {
    });
 }
 
-// function ingest_video (hatch, uri){
-//     return libingester.util.fetch_html(uri).then($ => {
-//
-//         const asset = new libingester.VideoAsset();
-//
-//         if (!$('script[type="application/ld+json"]').text()) { // Error 404
-//             return;
-//         }
-//         // Catch info video
-//         const video_json = JSON.parse($('script[type="application/ld+json"]').text());
-//
-//         const date = new Date(Date.parse(video_json.uploadDate));
-//         const title = video_json.name;
-//         const video_uri = video_json.embedUrl;
-//         const thumbnail = video_json.thumbnailUrl;
-//
-//
-//         const image = libingester.util.download_image(thumbnail);
-//         image.set_title(title);
-//         hatch.save_asset(image);
-//
-//         console.log('processing: ', title);
-//         asset.set_last_modified_date(date);
-//         asset.set_title(title);
-//         asset.set_canonical_uri(uri);
-//         asset.set_thumbnail(image)
-//         asset.set_download_uri(video_uri);
-//         hatch.save_asset(asset);
-//
-//        }).catch((err) => {
-//            console.log('Ingest video error: ', err);
-//            if (err.code==-1) { return ingest_video(hatch, uri); }
-//        });
-// }
-
-// function ingest_gallery(hatch, uri){
-//     return libingester.util.fetch_html(uri).then($ => {
-//         const asset = new libingester.NewsArticle();
-//         const author = $('meta[name="author"]').attr('content');
-//         let body = $('#gallery-thumbs');
-//         const title = $('meta[name="title"]').attr('content');
-//         const canonical_uri = $('link[rel="canonical"]').attr('href');
-//         let date = $('meta[name="pub_date"]').attr('content');
-//         date=new Date(Date.parse(date));
-//         const page = 'Spin';
-//         const read_more = `Original article at <a href="${canonical_uri}">${page}</a>`;
-//         const section = $(".breadcrumbs a").first().text();
-//         const synopsis = $('meta[name="description"]').attr('content');
-//         let thumbnail;
-//         const uri_main_image = $('meta[property="og:image"]').attr('content');
-//
-//         if (!body[0]) { // Error 404
-//             return;
-//         }
-//
-//         body.find('li').map((i, elem) => {
-//             const img = $(elem).find('img').first();
-//             const caption = img.attr('title');
-//             const image = `<img src="${img.attr('src')}" alt="${img.attr('alt')}">`;
-//             const figure = $(`<figure>${image}</figure>`);
-//             const figcaption = $(`<figcaption><p>${caption}</p></figcaption>`);
-//             const down_img = libingester.util.download_img($(figure.children()[0]));
-//             down_img.set_title(title);
-//             hatch.save_asset(down_img);
-//             $(elem).replaceWith(figure.append(figcaption));
-//         });
-//
-//         // Article Settings
-//         console.log('processing', title);
-//         asset.set_authors([author]);
-//         asset.set_lede(title);
-//         asset.set_date_published(date);
-//         asset.set_last_modified_date(date);
-//         asset.set_read_more_link(read_more);
-//         asset.set_section(section);
-//         asset.set_source(page);
-//         asset.set_synopsis(synopsis);
-//         asset.set_title(title);
-//         asset.set_body(body);
-//         asset.set_canonical_uri(uri);
-//         asset.set_title(title);
-//         asset.set_custom_scss(CUSTOM_CSS);
-//         asset.render();
-//         hatch.save_asset(asset);
-//
-//        }).catch((err) => {
-//            console.log('Ingest article error: ', err);
-//            if (err.code==-1) { return ingest_gallery(hatch, uri); }
-//        });
-// }
-
 function _fetch_all_links(links, max) {
     let all_links = []; // all links retrieved from all categories
     return Promise.all(links.map(link => libingester.util.fetch_html(link).then($ => {
@@ -253,12 +165,7 @@ function _fetch_all_links(links, max) {
 function main() {
     const hatch = new libingester.Hatch('alo_dokter', 'vi');
 
-    // ingest_article(hatch, 'http://www.alodokter.com/usir-bau-mulut-dengan-rutin-menggunakan-mouthwash')
-    //     .then(() => hatch.finish());
-
     _fetch_all_links(CATEGORY_LINKS, MAX_LINKS).then(links => {
-
-        // console.log(links);
         return Promise.all(links.map(uri => ingest_article(hatch, uri)))
             .then(() => hatch.finish());
     }).catch(err => {
